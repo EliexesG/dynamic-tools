aa2f5849b1c3a16219fc2309d18aaa69d47e3e48
EliexesG <107147734+EliexesG@users.noreply.github.com>
2023-09-17 19:15:15 -0600

    cambios en navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import './navbar.css';
 
 import Imagen from './Imagen';
@@ -12,37 +14,57 @@ from "@fortawesome/free-solid-svg-icons";
 
 import Link from 'next/link';
 
+import { useRef } from 'react';
+
 export default function Navbar () {
+
+  const collapseDiv = useRef(null);
+  const collapseButton = useRef(null);
+
+  const handleCollapse = (e) => {
+
+    collapseDiv.current.classList.remove('show', 'collapse')
+    collapseDiv.current.classList.add('collapsing')
+    collapseButton.current.classList.add('collapsed')
+    collapseButton.current.setAttribute('aria-expanded', false)
+
+    setTimeout(() => {
+      collapseDiv.current.classList.remove('collapsing')
+      collapseDiv.current.classList.add('collapse')    
+    }, 250);
+
+  }
+
   return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary position-fixed w-100 top-0 z-3 bg-gradient">
         <div className="container-fluid">
-        <Link className="navbar-brand align-items-center" href="/">
+        <Link className="navbar-brand align-items-center" href="/" onClick={handleCollapse}>
           <Imagen src={'/images/logos/small_size_logo.png'} alt="brand-logo" width={55} height={49} className="me-2 rounded"/>
           <span>A&M Dynamic Tools S.A.</span>
         </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button ref={collapseButton} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarOpciones" aria-controls="navbarOpciones" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse" id="navbarOpciones" ref={collapseDiv}>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" href="/">
+                <Link className="nav-link" aria-current="page" href="/" onClick={handleCollapse}>
                 <FontAwesomeIcon icon={faHouse} /> Inicio</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" href="/servicios">
+                <Link className="nav-link" href="/servicios" onClick={handleCollapse}>
                 <FontAwesomeIcon icon={faHandshakeAngle} /> Servicios</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" href="/maquinaria">
+                <Link className="nav-link" href="/maquinaria" onClick={handleCollapse}>
                 <FontAwesomeIcon icon={faScrewdriverWrench} /> Maquinaria</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" href="/nosotros">
+                <Link className="nav-link" href="/nosotros" onClick={handleCollapse}>
                 <FontAwesomeIcon icon={faUsers} /> Nosotros</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" href="/contactanos">
+                <Link className="nav-link" href="/contactanos" onClick={handleCollapse}>
                 <FontAwesomeIcon icon={faComment} /> Contactanos</Link>
               </li>
             </ul>
